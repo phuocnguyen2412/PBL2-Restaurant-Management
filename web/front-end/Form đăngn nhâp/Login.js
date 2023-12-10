f97c0659b96f46f6207e7e706e8fc3d1929a7c53
Phước Nguyên tới chơi :D <ttcenter@TTCenters-MBP.lan>
2023-12-10 16:16:10 +0700

    1

--- web/front-end/Form đăngn nhâp/Login.js
+++ web/front-end/Form đăngn nhâp/Login.js
@@ -1,43 +1,95 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
-function login(event) {
-  event.preventDefault();
 
-  let user = {
-    username: $("#username").value,
-    password: $("#password").value,
-  };
-  console.log(user);
-  fetch("http://localhost:5225/api/Auth/Login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(user),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      if (data.message == "1") {
-        window.location.href = "./web/front-end/Menu/Menu.html";
-      } else {
-        throw data.mesaage;
-      }
+function User(username, password) {
+  this.username = username;
+  this.password = password;
+  this.maNV = this.checkUser;
+  this.Login = function () {
+    fetch("http://localhost:5225/api/Auth/Login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: this.username,
+        password: this.password,
+      }),
     })
-    .catch((error) => {
-      console.error("Error:");
-      function showSuccessToast() {
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.message == "1") {
+          window.location.href = "./web/front-end/Menu/Menu.html";
+        } else {
+          throw data.message;
+        }
+      })
+      .catch((error) => {
         toast({
           title: "Thất bại!",
           message: "Tài khoản hoặc mật khẩu không đúng!",
           type: "error",
           duration: 5000,
         });
-      }
-      showSuccessToast();
-    });
+      });
+  };
+  this.checkUser = function () {
+    return fetch("http://localhost:5225/api/Auth/Login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: this.username,
+        password: this.password,
+      }),
+    }).then((response) => response.json());
+  };
+  this.changePassword = function () {
+    if (this.maNV && this.maNV != 0) {
+      $("#accout-change").innerHTML = `
+                <label for="new-password-change">Password mới:</label>
+                <input
+                    type="password"
+                    id="new-password-change"
+                    name="new-password-change"
+                    required
+                />
+            `;
+    }
+    this.password = $("#new-password-change").value;
+    fetch("http://localhost:5225/api/Auth/Login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: this.username,
+        password: this.password,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        toast({
+          title: "Thành công!",
+          message: "Đổi mật khẩu thành công!",
+          type: "error",
+          duration: 5000,
+        });
+      })
+      .catch((error) => {
+        toast({
+          title: "Thất bại!",
+          message: "Tài khoản hoặc mật khẩu không đúng!",
+          type: "error",
+          duration: 5000,
+        });
+      });
+  };
 }
 
-document.getElementById("submit-login-btn").onclick = function (event) {
-  login(event);
+$("#submit-login-btn").onclick = function (event) {
+  event.preventDefault();
+  const user = new User($("#username").value, $("#password").value);
+  user.Login();
 };
